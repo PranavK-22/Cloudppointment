90a7f4cf44a248bbb34233b7c623a090591e6a30
PranavK-22 <112797777+PranavK-22@users.noreply.github.com>
2025-04-14 15:34:58 +0530

    Update config.js

--- project-root/healthcare-frontend/src/config.js
+++ project-root/healthcare-frontend/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:5001"; // Change this if backend runs on a different port
+export const API_URL = "healthcare-backend-api-dgcagedvczemh4bf.canadacentral-01.azurewebsites.net"; // Change this if backend runs on a different port
